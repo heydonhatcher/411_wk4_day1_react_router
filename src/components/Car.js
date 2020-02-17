b2a4a27978fbb53e89c0a3ac529bf048cb48e29e
heydonhatcher <heyhatch@gmail.com>
2020-02-17 16:15:58 -0600

    ready to send along

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+import { Container, Paper, Chip, Typography } from "@material-ui/core";
 
-const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+const Car = props => {
+  const id = props.match.params.id;
+  const car = cars.find(car => car.id == id);
 
-export default Car
+  console.log(car);
+
+  return (
+    <div style={{ padding: "25px" }}>
+      <Container fixed maxWidth="sm">
+        <Typography variant="h3">{car.Name}</Typography>
+        <Paper
+          style={{
+            display: "flex",
+            justifyContent: "left",
+            flexWrap: "wrap",
+            padding: "10px"
+          }}
+          elevation={3}
+        >
+          {Object.keys(car).map((carkey, i) => {
+            return (
+              <Chip
+                style={{ padding: "5px" }}
+                size="small"
+                label={`${carkey}: ${car[carkey]}`}
+              />
+            );
+          })}
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+export default Car;
